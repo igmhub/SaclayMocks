7ce0f9a0182ecedc26b57df7f6a011d6d57bf371
londumas <helion331990@gmail.com>
2019-05-03 14:23:22 -0600

    travis testing suit

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import glob
+
+from setuptools import setup
+
+scripts = glob.glob('bin/*')
+
+description = "Simulated Absorption for Cosmology with Lyman-Alpha from the Yvette Mocks"
+
+version="0.1"
+setup(name="SaclayMocks",
+    version=version,
+    description=description,
+    url="https://github.com/igmhub/SaclayMocks",
+    author="Thomas Etourneau, Jean-Marc Le Goff et al",
+    author_email="<your email here>",
+    packages=['SaclayMocks'],
+    package_dir = {'': 'py'},
+    package_data = {'SaclayMocks': ['etc/']},
+    install_requires=['numpy','scipy','iminuit','healpy','fitsio',
+        'numba','future','setuptools'],
+    test_suite='SaclayMocks.test.test_cor',
+    scripts = scripts
+    )
