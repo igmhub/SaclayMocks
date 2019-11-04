b402a26eb8b33b74159b5d5dbd2eb284eeb6ad9f
Thomas <thomas.etourneau@cea.fr>
2019-11-04 00:54:43 -0800

    Added non linear effects in p1dmissing

--- bin/p1d_missing.py
+++ bin/p1d_missing.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from __future__ import division, print_function
-from SaclayMocks import powerspectrum
+from SaclayMocks import powerspectrum, util, constant
 import os
 import fitsio
 import numpy as np
@@ -12,13 +12,27 @@ from scipy import interpolate
 def P_RSD(k_par,k_perp,P, beta):
     k=np.sqrt(k_par*k_par+k_perp*k_perp)
     mu = k_par/np.maximum(k,1E-15)
-    #print("k_par=",k_par)
-    #print ("k_perp=",k_perp)
-    #print("k=",k)
-    #print("mu=",mu)
-    #print ((1+beta*mu**2)**2 * P(k))
-    #mu = k_perp/np.maximum(k,1E-15) # prov
     return (1+beta*mu**2)**2 * P(k)
+
+def D_Prats(k,mu,P,zref,Growth):
+    if (abs(zref-3.0)<0.0001):
+        q1 = 0.792; q2=0; k_p = 17.1; k_v = 1.16; a_v = 0.578; b_v = 1.63 # Planck Tab 7 
+    if (abs(zref-2.8)<0.0001):
+        q1 = 0.773; q2=0; k_p = 19.2; k_v = 1.16; a_v = 0.608; b_v = 1.65 # Planck Tab 7 
+    if (abs(zref-2.6)<0.0001):
+        q1 = 0.781; q2=0; k_p = 21.1; k_v = 1.15; a_v = 0.611; b_v = 1.64 # Planck Tab 7 
+    if (abs(zref-2.4)<0.0001):
+        q1 = 0.851; q2=0; k_p = 19.5; k_v = 1.06; a_v = 0.548; b_v = 1.61 # Planck Tab 7 
+        #q1 = -0.0020; q2=0.623; k_p = 10.9; k_v = 0.517; a_v = 0.152; b_v = 1.62 # Planck Tab 5 
+    if (abs(zref-2.2)<0.0001):
+        q1 = 0.867; q2=0; k_p = 19.4; k_v = 1.06; a_v = 0.514; b_v = 1.60 # Planck Tab 7 
+    Delta2 = k**3 * P(k) * Growth * Growth /2/np.pi**2
+    return np.exp ( (q1 * Delta2 + q2 * Delta2**2) * (1-(k/k_v)**a_v * mu**b_v ) - (k/k_p)**2 )
+
+def P_RSD_Prats(k_par,k_perp,P, beta,zref,Growth):
+    k=np.sqrt(k_par*k_par+k_perp*k_perp)
+    mu = k_par/np.maximum(k,1E-15)
+    return (1+beta*mu**2)**2 * P(k) * D_Prats(k,mu,P,zref,Growth)
 
 
 # def PW2(k, DX) :
@@ -56,10 +70,13 @@ parser.add_argument("--beta", type=float, required=True,
 parser.add_argument("--voxel-size", type=float, default=2.19, required=False,
     help="value of voxel-size (delta large scale)")
 
+parser.add_argument("--zref", type=float, default=2.2, required=False,
+    help="value of redshift")
+
 parser.add_argument("--k-max", type=float, default=20., required=False,
     help="kmax to compute the missing 1D power spectrum")
 
-parser.add_argument("--dk", type=float, default=0.01, required=False,
+parser.add_argument("--dk", type=float, default=0.001, required=False,
     help="dk to compute the missing 1D power spectrum")
 
 parser.add_argument("--plot-p1d", action='store_true', required=False,
@@ -67,6 +84,9 @@ parser.add_argument("--plot-p1d", action='store_true', required=False,
 
 parser.add_argument("--no-rsd", action='store_false', required=False,
     help="compute the missing 1D power spectrum without RSD")
+
+parser.add_argument("--linear", action='store_true', required=False,
+    help="only linear contributions")
 
 args = parser.parse_args()
 
@@ -77,18 +97,22 @@ DX = args.voxel_size
 beta = args.beta
 kmax = args.k_max
 dk = args.dk
+linear = args.linear # False if not specified
+zref = args.zref
 print("The missing 1D power spectrum will be saved in {}".format(outfile))
 print("It will be computed with "+
 "RSD={} and beta={}; voxcel={}; kmax={}; dk={}".format(RSD, beta, DX, kmax, dk))
-
-# dk=0.01;  # prov
+if not linear:
+    print("z_ref = {}".format(zref))
+    if zref > 3:
+        zref = 3
+        print("Redshift is set to {} for all z > 3".format(zref))
 PI = np.pi
 kk=np.arange(kmax/dk)*dk
 filename = os.path.expandvars("$SACLAYMOCKS_BASE/etc/PlanckDR12.fits")
 P_camb = powerspectrum.P_0(filename)
 Pcamb = P_camb.P(kk)
 kny = PI/DX
-# kp = np.arange(kny/dk)*dk
 kp = np.arange(PI/0.2/dk)*dk  # k for plot
 
 #.................................     compute P1D
@@ -99,7 +123,11 @@ if RSD:
     k_par_t = k_par.reshape(len(k_par),-1)
     k_perp = np.arange(kmax/dk)*dk
     k_perp_t = k_perp.reshape(len(k_perp),-1)
-    PRSD = P_RSD(k_par_t,k_perp,P_camb.P, beta)
+    if (linear) :
+        PRSD = P_RSD(k_par_t,k_perp,P_camb.P, beta)
+    else :
+        Growth = util.fgrowth(zref, constant.omega_M_0)
+        PRSD = P_RSD_Prats(k_par_t,k_perp,P_camb.P, beta, zref,Growth)
     P1DcambRSD = powerspectrum.P_1D_RSD(k_par,k_perp,PRSD).P1D(kp)
 
 #print ("0")
@@ -149,18 +177,18 @@ if RSD:
     P1DWcutcambRSD = powerspectrum.P_1D_RSD(k_par,k_perp,PRSD).P1D(kp)
 
 #.................................      missing P^1D(k)
-P1Dmissing = interpolate.InterpolatedUnivariateSpline(kp,P1Dcamb - P1DWcutcamb)
+P1Dmissing = interpolate.InterpolatedUnivariateSpline(kp, np.maximum(P1Dcamb - P1DWcutcamb, 0))
 
 if RSD:
-    P1DmissingRSD = interpolate.InterpolatedUnivariateSpline(kp,P1DcambRSD - P1DWcutcambRSD)
+    P1DmissingRSD = interpolate.InterpolatedUnivariateSpline(kp, np.maximum(P1DcambRSD - P1DWcutcambRSD, 0))
 
 # Write to fits file
 print("P1D computed. Writting file...")
 outfits = fitsio.FITS(outfile, 'rw', clobber=True)
-table = [kp, np.maximum(P1Dmissing(kp), 0)]
+table = [kp, P1Dmissing(kp)]
 names = ['k', 'P1Dmiss']
 if RSD:
-    table.append(np.maximum(P1DmissingRSD(kp), 0))
+    table.append(P1DmissingRSD(kp))
     names.append('P1DmissRSD')
 outfits.write(table, names=names, extname='P1D')
 outfits[-1].write_key('beta', beta)
@@ -186,8 +214,8 @@ if do_plots:
     ax.plot(kp, P1DcambRSD, color="green", label='PcambRSD')
     ax.plot(kp, P1DWcutcamb, color="blue", label='Pcamb_Wcut')
     ax.plot(kp, P1DWcutcambRSD, color="green", label='PcambRSD_Wcut')
-    ax.plot(kp, np.maximum(P1Dmissing(kp), 0), color="red", label='Pmissing')
-    ax.plot(kp, np.maximum(P1DmissingRSD(kp), 0), color="red", label='PmissingRSD')
+    ax.plot(kp, P1Dmissing(kp), color="red", label='Pmissing')
+    ax.plot(kp, P1DmissingRSD(kp), color="red", label='PmissingRSD')
     ax.plot(kk[i], Pk[i], color="orange", label='Pmiss_old')
     ax.set_xlabel('k [h/Mpc]')
     ax.grid()
